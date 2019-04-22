7c09b7b4ea8c9818347e48052f156b5ca40cd5df
Ricardo <ricardas828@gmail.com>
2019-04-22 19:23:13 +0200

    Molta molta merdeee, k difisiiil

---
+++ proyecto_1/app/src/main/java/com/l/marc/proyecto_1/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.l.marc.proyecto_1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000; // el tiempo de ejecución, que fasiiiil
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goMain = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(goMain);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
