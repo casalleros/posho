dcb56f6370328c24cf20a913e39d8dbcda36a2f6
ricardooo1998 <33457354+ricardooo1998@users.noreply.github.com>
2019-05-05 20:35:53 +0200

    corregir errores en el registro

--- proyecto_1/app/build/generated/source/r/debug/android/support/constraint/R.java
+++ proyecto_1/app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -67,20 +67,20 @@ public final class R {
         public static final int layout_optimizationLevel = 0x7f040150;
     }
     public static final class id {
-        public static final int bottom = 0x7f090025;
-        public static final int end = 0x7f09004a;
-        public static final int gone = 0x7f090063;
-        public static final int invisible = 0x7f09006e;
-        public static final int left = 0x7f090074;
-        public static final int packed = 0x7f09008b;
-        public static final int parent = 0x7f09008e;
-        public static final int percent = 0x7f090091;
-        public static final int right = 0x7f090096;
-        public static final int spread = 0x7f0900ba;
-        public static final int spread_inside = 0x7f0900bb;
-        public static final int start = 0x7f0900c0;
-        public static final int top = 0x7f0900d9;
-        public static final int wrap = 0x7f0900eb;
+        public static final int bottom = 0x7f090028;
+        public static final int end = 0x7f090053;
+        public static final int gone = 0x7f09006d;
+        public static final int invisible = 0x7f090078;
+        public static final int left = 0x7f09007e;
+        public static final int packed = 0x7f090096;
+        public static final int parent = 0x7f090099;
+        public static final int percent = 0x7f09009c;
+        public static final int right = 0x7f0900a4;
+        public static final int spread = 0x7f0900c8;
+        public static final int spread_inside = 0x7f0900c9;
+        public static final int start = 0x7f0900ce;
+        public static final int top = 0x7f0900ea;
+        public static final int wrap = 0x7f0900fc;
     }
     public static final class styleable {
         public static final int[] ConstraintLayout_Layout = { 0x010100c4, 0x0101011f, 0x01010120, 0x0101013f, 0x01010140, 0x7f040037, 0x7f040038, 0x7f040062, 0x7f040096, 0x7f040097, 0x7f04011c, 0x7f04011d, 0x7f04011e, 0x7f04011f, 0x7f040120, 0x7f040121, 0x7f040122, 0x7f040123, 0x7f040124, 0x7f040125, 0x7f040126, 0x7f040127, 0x7f040128, 0x7f040129, 0x7f04012a, 0x7f04012b, 0x7f04012c, 0x7f04012d, 0x7f04012e, 0x7f04012f, 0x7f040130, 0x7f040131, 0x7f040132, 0x7f040133, 0x7f040134, 0x7f040135, 0x7f040136, 0x7f040137, 0x7f040138, 0x7f040139, 0x7f04013a, 0x7f04013b, 0x7f04013c, 0x7f04013d, 0x7f04013e, 0x7f04013f, 0x7f040140, 0x7f040141, 0x7f040142, 0x7f040143, 0x7f040144, 0x7f040146, 0x7f040147, 0x7f040148, 0x7f040149, 0x7f04014a, 0x7f04014b, 0x7f04014c, 0x7f04014d, 0x7f040150 };
